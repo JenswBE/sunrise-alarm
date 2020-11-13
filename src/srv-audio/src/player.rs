2d7e794c7cc56f1812113315de7ae903e1660a79
Jens Willemsens <jens@jensw.be>
2020-11-13 16:53:52 +0100

    srv-audio: Add increase volume

--- src/srv-audio/src/player.rs
+++ src/srv-audio/src/player.rs
@@ -5,11 +5,13 @@ use std::thread;
 
 use tokio::sync::mpsc as tokio_mpsc;
 
+const VOLUME_STEP: f32 = 0.1;
 pub type Remote = tokio_mpsc::UnboundedSender<Command>;
 
 #[derive(Clone, Debug, PartialEq)]
 pub enum Command {
     Start,
+    IncreaseVolume,
     Stop,
 }
 
@@ -20,14 +22,22 @@ pub async fn setup() -> Remote {
         let (_stream, stream_handle) = rodio::OutputStream::try_default().unwrap();
         let mut sink = rodio::Sink::try_new(&stream_handle).unwrap();
         while let Ok(command) = srx.recv() {
-            if command == Command::Start {
-                let file = File::open("default.mp3").unwrap();
-                let source = rodio::Decoder::new_looped(BufReader::new(file)).unwrap();
-                sink = rodio::Sink::try_new(&stream_handle).unwrap();
-                sink.append(source);
-                sink.play();
-            } else {
-                sink.stop();
+            match command {
+                Command::Start => {
+                    let file = File::open("default.mp3").unwrap();
+                    let source = rodio::Decoder::new_looped(BufReader::new(file)).unwrap();
+                    sink = rodio::Sink::try_new(&stream_handle).unwrap();
+                    sink.append(source);
+                    sink.set_volume(VOLUME_STEP);
+                    sink.play();
+                }
+                Command::IncreaseVolume => {
+                    let new = sink.volume() + VOLUME_STEP;
+                    if new <= 1.0 {
+                        sink.set_volume(new)
+                    }
+                }
+                Command::Stop => sink.stop(),
             }
         }
     });
