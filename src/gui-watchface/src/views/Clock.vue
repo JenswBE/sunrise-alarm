bb3bfa725ddbb10ad0f54f4bd8ef9a781cd9ee7f
Jens Willemsens <jens@jensw.be>
2020-11-19 22:48:57 +0100

    gui-watchface: Add basic clock
    Based on https://github.com/dangvanthanh/vue-clock/

--- src/gui-watchface/src/views/Clock.vue
+++ src/gui-watchface/src/views/Clock.vue
@@ -2,8 +2,13 @@
   <v-container fluid fill-height>
     <v-row class="fill-height">
       <v-col align-self="end" cols="12" class="text-center">
-        <p class="text-h1 font-weight-bold">14:21</p>
-        <p class="text-h4">Woensdag 18 november 2020</p>
+        <p
+          class="text-h1 font-weight-bold"
+          style="font-family: DejaVu Sans Mono, monospace !important"
+        >
+          {{ hour }}{{ sep }}{{ minute }}
+        </p>
+        <p class="text-h4">{{ date }}</p>
       </v-col>
       <v-col align-self="end" cols="12" class="text-center">
         <p class="text-subtitle-1">Next alarm: Tomorrow at 00:00</p>
@@ -13,7 +18,42 @@
 </template>
 
 <script>
+import helpers from "../helpers";
+
 export default {
   name: "Clock",
+
+  data: () => ({
+    hour: "00",
+    minute: "00",
+    sep: ":",
+    date: "",
+  }),
+
+  methods: {
+    updateDateTime() {
+      const now = new Date();
+      this.hour = helpers.padTime(now.getHours());
+      this.minute = helpers.padTime(now.getMinutes());
+      this.sep = now.getSeconds() % 2 == 0 ? ":" : " ";
+      this.date = now.toLocaleDateString("en-UK", {
+        weekday: "long",
+        day: "numeric",
+        month: "long",
+        year: "numeric",
+      });
+      this.$options.timer = window.setTimeout(this.updateDateTime, 1000);
+    },
+  },
+
+  mounted() {
+    this.$options.timer = window.setTimeout(this.updateDateTime, 1000);
+    this.updateDateTime();
+  },
+
+  beforeDestroy() {
+    window.clearTimeout(this.$options.timer);
+    this.updateDateTime();
+  },
 };
 </script>
