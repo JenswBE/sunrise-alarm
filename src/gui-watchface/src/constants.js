f03d86f76e4e72ce5336e3a0fef750b9b46b781d
Jens Willemsens <jens@jensw.be>
2020-11-21 15:53:50 +0100

    gui-watchface: Fix Prettier setup

--- src/gui-watchface/src/constants.js
+++ src/gui-watchface/src/constants.js
@@ -1,18 +1,18 @@
 export const DAYS = [
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-    "Sunday",
-]
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+  "Sunday",
+];
 
 export const EMPTY_ALARM = {
-    enabled: false,
-    name: "",
-    hour: 0,
-    minute: 0,
-    days: [],
-    skip_next: false,
+  enabled: false,
+  name: "",
+  hour: 0,
+  minute: 0,
+  days: [],
+  skip_next: false,
 };
--- src/gui-watchface/src/helpers.js
+++ src/gui-watchface/src/helpers.js
@@ -1,7 +1,7 @@
 export default {
   padTime,
   formatTime,
-  scrollToTop
+  scrollToTop,
 };
 
 function padTime(value) {
