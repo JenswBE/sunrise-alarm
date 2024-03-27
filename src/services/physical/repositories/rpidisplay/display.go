b4e8339e8243162ffc13953c4c6dc4cf6a7324eb
Jens Willemsens <6514515+JenswBE@users.noreply.github.com>
2024-03-27 18:00:53 +0100

    Add workaround for screen glitches

--- src/services/physical/repositories/rpidisplay/display.go
+++ src/services/physical/repositories/rpidisplay/display.go
@@ -3,6 +3,7 @@ package rpidisplay
 import (
 	"fmt"
 	"os"
+	"path"
 
 	"github.com/JenswBE/sunrise-alarm/src/services/physical/repositories"
 	"github.com/rs/zerolog/log"
@@ -11,17 +12,33 @@ import (
 var _ repositories.Display = &RPiDisplay{}
 
 const (
-	brightnessFilePath = "/sys/class/backlight/10-0045/brightness"
-	minBrightness      = 7
-	maxBrightness      = 200
+	brightnessFileBasePath = "/sys/class/backlight"
+	minBrightness          = 7
+	maxBrightness          = 200
 )
 
 type RPiDisplay struct {
-	currentBrightness byte
-	brightnessFile    *os.File
+	currentBrightness  byte
+	brightnessFile     *os.File
+	brightnessFilePath string
 }
 
 func NewRPiDisplay() (*RPiDisplay, error) {
+	// Derive brightness file path
+	backlights, err := os.ReadDir(brightnessFileBasePath)
+	if err != nil {
+		return nil, fmt.Errorf("failed to list backlight folders in %s: %w", brightnessFileBasePath, err)
+	}
+	switch len(backlights) {
+	case 0:
+		return nil, fmt.Errorf("no backlight folders found in %s", brightnessFileBasePath)
+	case 2:
+		log.Warn().Str("base_path", brightnessFileBasePath).Any("folders", backlights).Msg("Multiple backlight folders found. Using first one")
+	}
+	brightnessFilePath := path.Join(brightnessFileBasePath, backlights[0].Name(), "brightness")
+
+	// Open brightness file
+	log.Info().Str("path", brightnessFilePath).Msg("Opening brightness file ...")
 	brightnessFile, err := os.Create(brightnessFilePath)
 	if err != nil {
 		log.Err(err).Str("path", brightnessFilePath).Msg("NewRPiDisplay: Failed to open brightness file")
@@ -32,7 +49,7 @@ func NewRPiDisplay() (*RPiDisplay, error) {
 
 func (d *RPiDisplay) SetBrightness(brightness byte) error {
 	// Setup logger
-	logger := log.With().Str("path", brightnessFilePath).Uint8("brightness", brightness).Logger()
+	logger := log.With().Str("path", d.brightnessFilePath).Uint8("brightness", brightness).Logger()
 
 	// Ensure brightness is within limits
 	switch {
